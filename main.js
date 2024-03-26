01b0055614dfe91bf3a08f4676262bfd36a8b979
adityaspande1 <121101849+adityaspande1@users.noreply.github.com>
2024-03-26 21:20:38 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// main.js
+import {add,sub,multiply,div} from './math'
+
+// Using math functions
+const a = 10;
+const b = 5;
+
+console.log("Addition:", add(a, b));
+console.log("Subtraction:", sub(a, b));
+console.log("Multiplication:", multiply(a, b));
+console.log("Division:", div(a, b));
